6d0fc8862cf72597b8da62cf76287447e3615d37
stuti <stutigoel22@gmail.com>
2024-07-12 20:30:16 +0530

    user model

---
+++ src/models/user.models.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+
+const userSchema= mongoose.Schema({
+    userName:{
+        type:String,
+        tolowercase:true,
+        trim:true
+    },
+    firstName:{
+        type:String,
+        required: true,
+        tolowercase:true,
+        trim:true
+    },
+    lastName:{
+        type:String,
+        required: true,
+        tolowercase:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required: true,
+        tolowercase:true,
+        unique:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required: true,
+        trim:true,
+        minLength:8
+    },
+    phoneNumber:{
+        type:String,
+        trim:true
+    },
+    refreshToken:{
+        type:String
+    },
+    isActive:{
+        type:Boolean,
+        default:false
+    }
+},{timeStamps:true}
+)
+
+
+export const User= mongoose.model('User',userSchema);
